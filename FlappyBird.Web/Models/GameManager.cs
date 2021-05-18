aeb4b52db5643d1bb5f0767370d32c540b36660a
MikeWhalenIII <michael1327@hotmail.com>
2021-05-17 22:57:48 -0400

    updates

---
+++ FlappyBird.Web/Models/GameManager.cs
@@ -0,0 +1,24 @@
+﻿namespace FlappyBird.Web.Models
+{
+    public class GameManager
+    {
+        private readonly int _gravity = 2;
+        
+        public BirdModel Bird { get; set; }
+        public bool IsRunning { get; set; } = false;
+
+        public GameManager()
+        {
+            Bird = new BirdModel();
+        }
+
+        public async void MainLoop()
+        {
+            IsRunning = true;
+            while(IsRunning)
+            {
+                Bird.Fall(2);
+            }
+        }
+    }
+}
